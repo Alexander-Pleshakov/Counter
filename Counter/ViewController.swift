9425125a9a5d749d2e7ca8a7312f3bbda994b1dc
Pleshakov-Alexandr <sanya.pleshakov.95@mail.ru>
2023-11-12 14:40:28 +0300

    Created label and button, added action on tap for button

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private var count = 0
+    
+    @IBOutlet weak var counterValueLabel: UILabel!
+    @IBOutlet weak var addButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func buttonAddOne(_ sender: Any) {
+        count += 1
+        counterValueLabel.text = "Значение счетчика: \(count)"
+    }
+    
 
 }
 
